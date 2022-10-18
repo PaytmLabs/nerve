7932186be3ec65b283885b18a1e96c0393bf40f9
TomasTorresB <tomas.torresbardavid@gmail.com>
2022-10-18 18:10:25 -0400

    NSE scripts helper methods refactor. Add intensiry validation for NSE scripts.

--- bin/attacker.py
+++ bin/attacker.py
@@ -1,13 +1,12 @@
 import time
 import threading
 
-from core.manager   import rule_manager
-from core.parser    import ConfParser
-from core.logging   import logger
-from core.redis     import rds
-from core.parser    import ScanParser
+from core.manager     import rule_manager
+from core.parser      import ConfParser
+from core.logging     import logger
+from core.redis       import rds
+from core.nse_scripts import check_rule, get_metadata
 
-import nmap
 import os
 import sys
 import pip
@@ -21,8 +20,7 @@ def run_python_rules(conf):
   """
    Launch python rules according to config
 
-   Parameters: 
-     conf (dict): Scan configuration variable. Used to know which rules to run.
+   :param conf dict: Scan configuration variable. Used to know which rules to run.
   """
 
   data = rds.get_scan_data()
@@ -57,166 +55,44 @@ def run_python_rules(conf):
             thread = threading.Thread(target=rule.check_rule, args=(ip, port, values, conf))
             thread.start()
 
-# [FALTA INCORPORAR CONFIG]
-def run_NSE_rules():
+def run_NSE_rules(conf):
   """
-   Launch lua rules according to config
+   Launch NSE rules according to config (??)
   """
   # Redis data
   data = rds.get_scan_data()
   #exclusions = rds.get_exclusions() # Not implemented for nmap yet
-  nm = nmap.PortScanner()
   
-  # Nmap args
-  path_to_scripts = os.environ['Nmap_scripts_path']
-  scripts_names = ['ftp-brute.nse','ftp-steal.nse']
-  #scripts_args = "--script-args user=ftp_user,pass=ftp_user,dir=files"
-  scripts_args = "--script-args brute.credfile=/home/ubuntu/Documents/creds.txt,user=ftp_user,pass=ftp_user,dir=files"
-
+  # HARD CODED FOR NOW, SHOULD USE CONF IN THE FUTURE? 
+  scripts_names = ['ftp-brute','ftp-steal']
+  scripts_args = "brute.credfile={},user=ftp_user,pass=ftp_user,dir=files".format(os.environ['credfile_path'])
   #logger.debug('Scripts to be executed: {}'.format(scripts_path))
   #logger.debug('Scripts args: {}'.format(scripts_args))
 
-  # Nmap doesn't support multiple ports with different ports on one command.
+  # Nmap doesn't support multiple scripts with different ports on one command.
   # Therefore multiple commands are ran in parallel for each port of each host.
-  # Note: It is not possible to map a script to a specific port but not the other scripts to that port as well. Thus all scripts are executed for each port,ip combination.
   # For each host launch a new attack thread
   for ip, values in data.items():
-
     if 'ports' in values and len(values['ports']) > 0:  
       logger.info('Attacking Ports: {} of asset: {}'.format(values['ports'], ip))
 
-      # Start new thread with NSE script
-      thread = threading.Thread(target=NSE_rules, args=(nm, ip, values, scripts_names, path_to_scripts, scripts_args))
-      thread.start()
+      # Filter scripts with higher intensity
+      for script in scripts_names:
+        metadata = get_metadata(script)
+        logger.debug('Metadata: ' + str(metadata))
 
+        logger.debug('Config intensity: ' + str(conf['config']['allow_aggressive']))
+        logger.debug('Rule intensity: ' + str(metadata['intensity']))
+        if conf['config']['allow_aggressive'] >= metadata['intensity']:
+         
+          # Start new thread for each NSE script
+          thread = threading.Thread(target=check_rule, args=(script, scripts_args, metadata, ip, values, conf))
+          thread.start()
 
-def NSE_rules(nm, ip, values, scripts_names, path_to_scripts, scripts_args):
-  """
-   Execute nse rules for host on ports.
-
-   Parameters:
-     nm (object): Nmap Portscanner object. Can execute nse scripts.
-     ip (string): Ip of host
-     values (dict): Related info to host
-     scripts_names (list): List of scripts names to execute
-     path_to_scripts (string): Path of nmap scripts folder
-     scripts_args (string): Arguments used by scripts
-  """
-
-  # Get ports to attack
-  ports = ','.join([str(p) for p in values['ports']])
- 
-  # Get scripts to execute
-  # Path should be hidden better, or execute script installed in nerve!
-  scripts_path = "" 
-  for n in scripts_names:
-    scripts_path += path_to_scripts + n + ','
-  scripts_path = scripts_path[:-1] # do not use last ,
-  scripts_syntax = "--script " + scripts_path # 
-  
-  # Start scan 
-  nm.scan(ip, ports=ports, arguments='{} {}'.format(scripts_syntax, scripts_args)) 
-  # logger.info('Scan command: ' + str(nm.command_line()))
-
-  # Check if the host has not been  switched off in the middle of scan 
-  test_scan_finished = nm.all_hosts()
-  test_scan_finished_len = len(test_scan_finished)
-  if test_scan_finished_len == 0:
-    logger.info('Error during scan, host switched off')
-  else:
-
-    # Scan finished
-    output_scan = nm._scan_result['scan'][ip]
-    logger.info(output_scan)
-    logger.info(output_scan.keys())
-    logger.info('-----')
-    
-    #Check if NSE script was executed correctly
-    for p in values['ports']:
-      if 'script' in output_scan['tcp'][p]:
-
-        # key is the script
-        for key,result in output_scan['tcp'][p]['script'].items():
-          logger.info('Sucessful scan')
-          logger.info('Script {}, Output: {}'.format(key,result))
- 
-          # Obtain metadata of script from nse file
-          nse_script = open(path_to_scripts + key + '.nse', 'r') 
-          description = ''
-          description_found = False
-          description_done = False
-          severity_level = 5 # PORQUE 5?
-          severity_level_found = False
-          confirm_description = ''
-          confirm_found = False
-          mitigation_description = ''
-          mitigation_found = False
-
-          #logger.info('Before traversing file')
-          for line in nse_script:
-            # All info has been found
-            if description_done and severity_level_found and confirm_found and mitigation_found:
-              break
-            # Case when description is being read
-            elif description_found and not description_done:
-              if ']]' in line:
-                description_done = True
-              else:
-                description += line
-            # Info is missing and description is not being read
-            else:             
-              if 'description' == line[:11]:
-                description_found = True
-              if 'severity' == line[:8]: 
-                line = line.replace(" ","")
-                severity_level = int(line[-2])
-                severity_level_found = True
-              if 'confirm' == line[:7]: 
-                 confirm_description = line.split('"')[1]
-                 confirm_found = True
-              if 'mitigation' == line[:10]:
-                 mitigation_description = line.split('"')[1]
-                 mitigation_found = True
-      
-          # Debug
-          #logger.debug('Description: {}'.format(description))
-          #logger.debug('Severity: {}'.format(severity_level))
-          #logger.debug('Confirm: {}'.format(confirm_description))
-          #logger.debug('Mitigation: {}'.format(mitigation_description))
-          #logger.debug('--------')         
-          #logger.debug('Description_found: {}'.format(description_found))
-          #logger.debug('Description_done: {}'.format(description_done))
-          #logger.debug('Severity_found: {}'.format(severity_level_found))
-          #logger.debug('Confirm_found: {}'.format(confirm_found))
-          #logger.debug('Mitigation_found: {}'.format(mitigation_found))
-     
-          # Obtain domain from parser
-          parser = ScanParser(p, values)
-          domain = parser.get_domain()
-        
-          # Save results on redis
-          rds.store_vuln({
-           'ip':ip,                                            # Check
-           'port':p,                                           # Check
-           'domain':domain,                                    # Check, es None en el caso que no halla
-           'rule_id':key,                                      # A medias, Corresponde a un código de 8 caracteres. Sin embargo, creo que lo único que se hace con este es realizar un hash más adelante y no es relevante el largo. Por ahora para identificar cada script se usara el nombre. En el caso de múltipels scripts esto fallaría.
-           'rule_sev':severity_level,                          # Check, usar campo 'severity' de scripts nse
-           'rule_desc':description,                            # Check, Usar descripción del script de nmap
-           'rule_confirm': confirm_description,                # Check, Descripción de algo, falta identificar de que, se puede dejar como string vacío supongo
-           'rule_details': result,                             # Check, Resultados del script
-           'rule_mitigation': mitigation_description           # Check, Descripción breve de como evitar el problema, permite string vacío creo
-          })
-
-      # Scripts were not executed correctly
-      else:
-        logger.debug('Error while executing scripts')
-        logger.debug('Error for scripts {} on host {}, port {}'.format(scripts_names, ip, p))
-
-  return
 
 def attacker():
   """
-   Daemon, launches scans.
+   Daemon, always running. Launches scans.
   """
 
   count = 0
@@ -230,7 +106,7 @@ def attacker():
       continue
     
     #run_python_rules(conf)
-    run_NSE_rules()
+    run_NSE_rules(conf)
     count += 1
       
     if count == conf['config']['scan_opts']['parallel_attack']:
---
+++ core/nse_scripts.py
@@ -0,0 +1,161 @@
+import sys
+# Dotenv no quedo instalado en el path de las otras librerías, por lo que hay que añadir el path. En al instalación original no debería ocurrir este problema.
+sys.path.append('/home/ubuntu/.local/lib/python3.6/site-packages')
+from dotenv import load_dotenv
+
+from core.parser import ScanParser
+from core.logging import logger
+from core.redis import rds
+
+
+import os
+import nmap
+
+load_dotenv()
+
+def check_rule(script, script_args, metadata, ip, values, conf):
+  """
+   Launch attack to service
+
+   :param script str: Script name
+   :param scripts str: Scripts args [Actualmente se pueden ingresar args de otros scripts, lo que no tiene sentido]
+   :param metadata dict(str or int): Metadata of script
+   :param ip str: Host ip
+   :param values dict(str): Port scan info
+   :param conf dict(str): Scan configuration info 
+  """
+  nm = nmap.PortScanner() 
+
+  script_syntax = '--script ' + os.environ['nmap_scripts_path'] + script + '.nse'
+  script_args_syntax = '--script-args ' + script_args  
+  ports = ','.join([str(p) for p in values['ports']])
+
+  # Start scan
+  nm.scan(ip, ports=ports, arguments='{} {}'.format(script_syntax, script_args_syntax))
+
+  # Check if the host is switched off in the middle of scan 
+  test_scan_finished = nm.all_hosts()
+  test_scan_finished_len = len(test_scan_finished)
+  if test_scan_finished_len == 0:
+    logger.info('Error during scan, host switched off')
+  else:
+ 
+    # Scan finished
+    output_scan = nm._scan_result['scan'][ip]
+    logger.debug('Raw Scan Output: ' + str(output_scan))
+ 
+    #Check if NSE script was executed correctly
+    for p in values['ports']:
+      if 'script' in output_scan['tcp'][p]:
+ 
+        # key = script
+        for key,result in output_scan['tcp'][p]['script'].items():
+          logger.info('Sucessful scan')
+          logger.debug('Script {}, Output: {}'.format(key,result))
+ 
+          # Save result in redis for further display
+          save_result(key, result, metadata, ip, p, values)
+      
+      # Script not executed correctlt
+      else:
+        logger.debug('Error while executing scripts')
+        logger.debug('Error for script {} on host {}, port {}'.format(script, ip, p))
+             
+
+  return
+
+def get_metadata(script):
+  """
+   Read through nse file to obtain corresponding metadata
+
+   :param scripts str: Script name
+   :return result dict(str or int): Metadata values found
+
+  """
+  try:
+    script_path = os.environ['nmap_scripts_path'] + script + '.nse'
+    logger.debug('Script path: {}'.format(script_path))
+    nse_script = open(script_path, 'r')
+
+    # Delimeters
+    description_found = False
+    description_done = False
+    severity_level_found = False
+    confirm_found = False
+    mitigation_found = False
+    intensity_found = False
+
+    # Info
+    description = ''
+    severity_level = 5 # 5 corresponds to undefined
+    confirm_description = ''  
+    mitigation_description = ''
+    intensity = 0 # Lowest possible, always executes script
+
+    # Traverse file
+    for line in nse_script:
+      # All info has been found
+      if description_done and severity_level_found and confirm_found and mitigation_found and intensity_found:
+        break
+      # Case when description is being read
+      elif description_found and not description_done:
+        if ']]' in line:
+          description_done = True
+        else:
+          description += line
+      # Info is missing and description is not being read
+      else:
+        if 'description' == line[:11]:
+          description_found = True
+        if 'severity' == line[:8]:
+          line = line.replace(" ","")
+          severity_level = int(line[-2])
+          severity_level_found = True
+        if 'confirm' == line[:7]:
+          confirm_description = line.split('"')[1]
+          confirm_found = True
+        if 'mitigation' == line[:10]:
+          mitigation_description = line.split('"')[1]
+          mitigation_found = True
+        if 'intensity' == line[:9]:
+          intensity = int(line[-2])
+          intensity_found = True
+  
+    result = {'description': description, 'severity_level': severity_level, 'confirm': confirm_description, 'mitigation': mitigation_description, 'intensity': intensity}
+    return result
+  
+  # Error when reading file
+  except IOError as e:
+    return {'error': e}
+
+
+def save_result(script, result, metadata, ip, port, values):
+  """
+   Save scan result in Redis.
+
+   :param script str: Script name
+   :param result str: Scan result
+   :param metadata dict(str or int): Metadata of script
+   :param ip str: Host ip
+   :param port str: Host port
+   :param values dict(str): Previous port scan info.
+  """ 
+
+  # Obtain domain from parser
+  parser = ScanParser(port, values)
+  domain = parser.get_domain()
+
+  # Save results on redis
+  rds.store_vuln({
+    'ip':ip,                                                 # Check
+    'port':port,                                             # Check
+    'domain':domain,                                         # Check, es None en el caso que no halla
+    'rule_id':script,                                        # A medias, Corresponde a un código de 8 caracteres. Sin embargo, creo que lo úni      co que se hace con este es realizar un hash más adelante y no es relevante el largo. Por ahora para identificar cada script se usara el nombre.       CREO QUE ESTO PUEDE FALLAR EN ALGUNOS CASOS.
+    'rule_sev': metadata['severity_level'],                  # Check, usar campo 'severity' de scripts nse
+    'rule_desc': metadata['description'],                    # Check, Usar descripción del script de nmap
+    'rule_confirm': metadata['confirm'],                     # Check, Descripción de algo, falta identificar de que, se puede dejar como strin      g vacío supongo
+    'rule_details': result,                                  # Check, Resultados del script
+    'rule_mitigation': metadata['mitigation']                # Check, Descripción breve de como evitar el problema, permite string vacío creo
+          })
+
+  return
